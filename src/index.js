c8e707ab656845d4cdd0c88998d3ba9178cd756d
bongoman-by <bongoman@tut.by>
2020-10-11 15:46:31 +0300

    feat: done Towel Sort  task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    let newArray = [];
+    
+    function isNan(value) {
+        return value !== value;
+      }
+    
+      if (matrix === null || matrix === undefined || isNan(matrix)) {
+        return newArray;
+      }
+    
+      if (matrix.constructor !== Array) {
+        return newArray;
+      }
+
+    matrix.forEach((array, index) => {
+  
+    if ((index + 1) % 2 === 0) {
+      newArray.push(array.sort((a, b) => b - a));
+    } else {
+        newArray.push(array);  
+    }
+  });
+    return [].concat(...newArray);
+  };
